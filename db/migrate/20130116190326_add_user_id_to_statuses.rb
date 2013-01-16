dab252e27f6063ad6dfaadd0148ac6efe8da2f83
Bob Bhatti <bobbhatti@simransservices.co.uk>
2013-01-16 19:07:40 +0000

    install devise and devise views

---
+++ db/migrate/20130116190326_add_user_id_to_statuses.rb
@@ -0,0 +1,8 @@
+class AddUserIdToStatuses < ActiveRecord::Migration
+  def change
+  		add_column :statuses, :user_id, :integer
+  		add_index :statueses, :user_id
+  		remove_column :statuses, :name
+
+  end
+end
